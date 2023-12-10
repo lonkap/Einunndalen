8e97320f4d42d28b36d74d601e145d0845920bb5
lonkap <petra.lonk@gmail.com>
2023-12-10 22:23:21 +0100

    novy

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+  //Add a new passthrough file copyfor JavaSceipt files
+  eleventyConfig.addPassthroughCopy('js');
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
