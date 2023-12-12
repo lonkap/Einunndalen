1794a8945136d5f9c17b3818165a56e94fe94cba
lonkap <petra.lonk@gmail.com>
2023-12-12 20:14:28 +0100

    js

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function(eleventyConfig) {
 	// Zkopírovat css/ to _site/css/
 	eleventyConfig.addPassthroughCopy("css");
   //Add a new passthrough file copyfor JavaSceipt files
-  eleventyConfig.addPassthroughCopy("fslightbox.js");
+  eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy({"favicon/*.*":"/"});
 
   return {
